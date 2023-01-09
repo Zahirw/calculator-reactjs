d630dae62b3a768db698ae5e862957b7ea3f547e
Zahirw <wakidzahir@gmail.com>
2023-01-09 19:35:41 +0700

    add handle error 2 operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,95 @@
 
 /* eslint no-eval: 0 */
-import { useState } from "react";
-// import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-  // decrement,
-  // increment,
-  // incrementByAmount,
-  // incrementAsync,
-  // selectCount,
+  input,
+  reset,
+  selectEntry,
 } from './features/counterSlice';
 import "./app.scss"
 
 function App() {
-  // const count = useSelector(selectCount);
-  // const dispatch = useDispatch();
-  const [number, setNumber] = useState('')
-  // const [entry, setEntry] = useState([])
+  const entry = useSelector(selectEntry);
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   console.log(entry)
-  // },[entry])
+  const handleInput = (param) => {
+    dispatch(input(param))
+  }
+
+  const handleReset = () => {
+    dispatch(reset())
+  }
   return (
     <div className="container">
       <div className="calc">
         <div className="input">
-          {number}
+          {entry}
         </div>
         <div className="result">
           <div>
             =
           </div>
           <div>
-          {['*', '+', '-', '/'].includes(number.charAt(number.length-1)) ? '' : eval(number)}
+          {['*', '+', '-', '/'].includes(entry.charAt(entry.length-1)) ? '' : eval(entry)}
           </div>
         </div>
         <hr className="separator"/>
         <div className="button-group">
           <div className="header-button">
-            <button className="btn-clear" onClick={() => setNumber('')}>
+            <button className="btn-clear" onClick={handleReset}>
               CLEAR
             </button>
-            <button className="btn-operator" onClick={() => setNumber((prev) => (prev+'*'))}>
+            <button className="btn-operator" onClick={() => handleInput('*')}>
               x
             </button>
           </div>
           <div className="body-button">
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+1))}>
+            <button className="btn-numeric" onClick={() => handleInput(1)}>
               1
             </button>
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+2))}>
+            <button className="btn-numeric" onClick={() => handleInput(2)}>
               2
             </button>
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+3))}>
+            <button className="btn-numeric" onClick={() => handleInput(3)}>
               3
             </button>
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+'+'))}>
+            <button className="btn-operator" onClick={() => handleInput('+')}>
               +
             </button>
           </div>
           <div className="body-button">
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+4))}>
+            <button className="btn-numeric" onClick={() => handleInput(4)}>
               4
             </button>
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+5))}>
+            <button className="btn-numeric" onClick={() => handleInput(5)}>
               5
             </button>
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+6))}>
+            <button className="btn-numeric" onClick={() => handleInput(6)}>
               6
             </button>
-            <button className="btn-operator" onClick={() => setNumber((prev) => (prev+'-'))}>
+            <button className="btn-operator" onClick={() => handleInput('-')}>
               -
             </button>
           </div>
           <div className="body-button">
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+7))}>
+            <button className="btn-numeric" onClick={() => handleInput(7)}>
               7
             </button>
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+8))}>
+            <button className="btn-numeric" onClick={() => handleInput(8)}>
               8
             </button>
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+9))}>
+            <button className="btn-numeric" onClick={() => handleInput(9)}>
               9
             </button>
-            <button className="btn-operator" onClick={() => setNumber((prev) => (prev+'/'))}>
+            <button className="btn-operator" onClick={() => handleInput('/')}>
               /
             </button>
           </div>
           <div className="body-button">
-            <button className="btn-operator" onClick={() => setNumber((prev) => ('-'+prev))}>
+            <button className="btn-operator" onClick={() => handleInput('-')}>
               +/-
             </button>
-            <button className="btn-numeric" onClick={() => setNumber((prev) => (prev+0))}>
+            <button className="btn-numeric" onClick={() => handleInput(0)}>
               0
             </button>
             <button className="btn-numeric">
--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -3,27 +3,24 @@ import { createSlice } from '@reduxjs/toolkit'
 export const counterSlice = createSlice({
   name: 'entry',
   initialState: {
-    value: 0,
+    value: '',
   },
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
+    input: (state, action) => {
+      if (['*', '+', '-', '/'].includes(state.value.charAt(state.value.length-1)) && ['*', '+', '-', '/'].includes(action.payload)) {
+        state.value = state.value.slice(0, state.value.length-1)+action.payload
+      } else {
+        state.value += action.payload
+      }
     },
-    // decrement: (state) => {
-    //   state.value -= 1
-    // },
-    // incrementByAmount: (state, action) => {
-    //   state.value += action.payload
-    // },
+    reset: (state) => {
+      state.value = ''
+    },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
-export const selectCount = (state) => state.entry.value
+export const { input, reset } = counterSlice.actions
+export const selectEntry = (state) => state.entry.value
 
 export default counterSlice.reducer
